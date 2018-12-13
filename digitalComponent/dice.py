9af9dd05a44994356a0772c8958b5976ba2d4991
GwnLars <43271054+GwnLars@users.noreply.github.com>
2018-12-13 13:01:03 +0100

    Added Dice sum and "animation"
    + dice sum for group1 and 2

--- digitalComponent/dice.py
+++ digitalComponent/dice.py
@@ -2,53 +2,53 @@ from items import*
 import random
 
 class dice(clickable):
-    def __init__(self,x,y,w,h,name, val):
+    def __init__(self,x,y,w,h,name, val,stopcount=20):
         super(dice, self).__init__(x,y,w,h,name)
         self.val=val
+        self.clicked = False
+        self.counter = 0
+        self.stopcount = stopcount
         
     def display(self): 
         fill(255)
-        if self.val == 1:
-            self.diceOne()
-        if self.val == 2:
-            self.diceTwo()
-        if self.val == 3:
-            self.diceThree()
-        if self.val == 4:
-            self.diceFour()
-        if self.val == 5:
-            self.diceFive()
-        if self.val == 6:
-            self.diceSix()
-    
+        if self.clicked:
+            getattr(self,"dice{}".format(random.randint(1,6)))()
+            self.counter += 1
+        else:
+            getattr(self,"dice{}".format(self.val))()
+        if self.counter >= self.stopcount:
+            self.counter = 0
+            self.clicked = False
+                         
     def onClick(self, *args):
         self.diceRoll()
-    
+        self.clicked = True
+        
     def onHover(self):
         pass
                 
     def diceRoll(self):
         self.val = random.randint(1,6) 
     
-    def diceOne(self):
+    def dice1(self):
         rect (self.x,self.y,self.w,self.h,10,10,10,10)
         fill(10)
         ellipse(self.x + 0.5*self.w,self.y+0.5*self.h,0.2*self.w,0.2*self.h)
         
-    def diceTwo(self):
+    def dice2(self):
         rect(self.x,self.y,self.w,self.h,10,10,10,10)
         fill(10)
         ellipse(self.x + 0.25 * self.w, self.y + 0.75 * self.h, 0.2*self.w, 0.2*self.h)
         ellipse(self.x + 0.75 * self.w, self.y + 0.25 * self.h, 0.2*self.w, 0.2*self.h)
         
-    def diceThree(self):
+    def dice3(self):
         rect (self.x,self.y,self.w,self.h,10,10,10,10)
         fill(10)
         ellipse(self.x + 0.25 * self.w, self.y + 0.75 * self.h, 0.2*self.w, 0.2*self.h)
         ellipse(self.x + 0.5*self.w,self.y+0.5*self.h,0.2*self.w,0.2*self.h)
         ellipse(self.x + 0.75 * self.w, self.y + 0.25 * self.h, 0.2*self.w, 0.2*self.h)
     
-    def diceFour(self):
+    def dice4(self):
         rect (self.x,self.y,self.w,self.h,10,10,10,10)
         fill(10)
         ellipse(self.x + 0.25 * self.w, self.y + 0.25 * self.h, 0.2*self.w, 0.2*self.h)
@@ -57,7 +57,7 @@ class dice(clickable):
         ellipse(self.x + 0.75 * self.w, self.y + 0.75 * self.h, 0.2*self.w, 0.2*self.h)
         
         
-    def diceFive(self):
+    def dice5(self):
         rect (self.x,self.y,self.w,self.h,10,10,10,10)
         fill(10)
         ellipse(self.x + 0.25 * self.w, self.y + 0.25 * self.h, 0.2*self.w, 0.2*self.h)
@@ -67,7 +67,7 @@ class dice(clickable):
         ellipse(self.x + 0.5*self.w,self.y+0.5*self.h,0.2*self.w,0.2*self.h)
         
         
-    def diceSix(self): 
+    def dice6(self): 
         rect (self.x,self.y,self.w,self.h,10,10,10,10)
         fill(10)
         ellipse(self.x + 0.25 * self.w, self.y + 0.25 * self.h, 0.2*self.w, 0.2*self.h)
@@ -83,19 +83,28 @@ class diceGroup(clickable):
         self.dice=list()
         for d in dice: 
             self.dice.append(d)
+        self.out = 0
+        self.outObservers = []
             
     def display(self):
         for d in self.dice:
             d.display()
     
     def onClick(self):
+        self.out = 0
         for d in self.dice:
             d.onClick()
+        for d in self.dice:
+            self.out += d.val
+        for callback in self.outObservers:
+            callback(self)
+        
     
     def addDice(self,d):
         if isinstance(dice,d):
             self.dice.append(d)
             
-
+    def bindTo(self,callback):
+        self.outObservers.append(callback)
         
         
